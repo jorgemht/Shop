b4d7a5051812ab59921beb36e4e201f89516c2a3
jorgemhtdev <jorge@jorgemht.dev>
2019-09-02 22:32:07 +0200

    First build

---
+++ Shop/Shop/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Shop
+{
+    using System.ComponentModel;
+    using Xamarin.Forms;
+
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+    }
+}
---
+++ Shop/Shop/Views/PanelPopupView.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace Shop.Views
+{
+    using System;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PanelPopupView : ContentView
+    {
+        public delegate void TapDelegate();
+
+        public PanelPopupView()
+        {
+            InitializeComponent();
+            GoToState("Collapsed");
+        }
+
+        public int HeaderHeight { get; private set; } = 70;
+
+        public TapDelegate OnExpandTapped { get; set; }
+        public TapDelegate OnCollapseTapped { get; set; }
+
+        private void ExpandTapped(object sender, EventArgs e)
+        {
+            GoToState("Expanded");
+            OnExpandTapped?.Invoke();
+        }
+
+        private void CollapseTapped(object sender, EventArgs e)
+        {
+            GoToState("Collapsed");
+            OnCollapseTapped?.Invoke();
+        }
+        private void GoToState(string visualState)
+        {
+            //VisualStateManager.GoToState(BasketSummary, visualState);
+            //VisualStateManager.GoToState(CollapseButton, visualState);
+            //VisualStateManager.GoToState(ExpandButton, visualState);
+        }
+    }
+}
